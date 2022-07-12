b6d0ecbb1c6d772e0b7f356afd1fd930b9e89bff
GabrielleMedeirosOliveira <gabriellemedeiros51@gmail.com>
2022-07-12 18:28:17 -0300

    Fix: Style login PJ

--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -1,77 +1,76 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100vh;
+  min-height: 100vh;
 
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 
-    background: var(--background-footer);
-    color: var(--background-company);
-`
+  background: var(--background-footer);
+  color: var(--background-company);
+`;
 
 export const Form = styled.form`
-    
+  display: flex;
+  flex-direction: column;
+
+  padding: 38px 18px 24px 18px;
+  gap: 10px;
+
+  background-color: var(--background-header-home);
+
+  border-radius: 4px;
+  box-shadow: var(--box-shadow);
+  h1 {
     display: flex;
-    flex-direction: column;
-    
-    padding: 38px 18px 24px 18px;
-    gap: 10px;
+    justify-content: center;
+  }
+  button {
+    margin-top: 8px;
+    margin-bottom: 8px;
 
-    background-color: var(--background-header-home);
+    background: var(--color-button-two);
+    color: var(--white);
+    font-weight: 600;
 
-    border-radius: 4px; 
-    box-shadow: var(--box-shadow);
-    h1{
-        display: flex;
-        justify-content: center;
+    border: 1px solid var(--color-primary-dark);
+    border-radius: 4px;
+
+    width: 259.9px;
+    height: 38.5px;
+
+    &:hover {
+      background: var(--color-button-home-hover);
     }
-    button {
-        margin-top: 8px;
-        margin-bottom: 8px;
+  }
 
-        background: var(--color-button-two);
-        color: var(--white);
-        font-weight: 600;
+  input {
+    background-color: #262626;
+    color: var(--white);
+    font-weight: 600;
 
-        border: 1px solid var(--color-primary-dark);
-        border-radius: 4px;
+    align-items: center;
 
-        width: 259.9px;
-        height: 38.5px;
+    padding: 0.3rem;
 
-        &:hover{
-            background: var(--color-button-home-hover);
-        }
+    border: 1px solid var(--color-primary-dark);
+    border-radius: 4px;
+
+    width: 264.66px;
+    height: 38.5px;
+
+    &::placeholder {
+      color: var(--white);
+      font-weight: 600;
     }
-
-    input {
-        background-color: #262626;
-        color: var(--white);
-        font-weight: 600;
-
-        align-items: center;
-
-        padding: .3rem;
-
-        border: 1px solid var(--color-primary-dark);
-        border-radius: 4px;
-
-        width: 264.66px;
-        height: 38.5px;
-        
-        &::placeholder {
-            color: var(--white);
-            font-weight: 600;
-        }
-    }
-`
+  }
+`;
 export const ContainerMessage = styled.div`
-    text-align: center;
-    width: 100%;
-    strong {
-        font-weight: 500;
-    }
-`
+  text-align: center;
+  width: 100%;
+  strong {
+    font-weight: 500;
+  }
+`;
--- src/pages/LoginPartiner/style.js
+++ src/pages/LoginPartiner/style.js
@@ -1,73 +1,74 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100vh;
+  min-height: 100vh;
 
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 
-    background: var(--color-primary);
-    color: var(--color-primary);
-`
+  background: var(--background-footer);
+  color: var(--background-company);
+`;
 export const Form = styled.form`
-    
+  display: flex;
+  flex-direction: column;
+
+  padding: 38px 18px 24px 18px;
+  gap: 10px;
+
+  background-color: var(--background-header-home);
+
+  border-radius: 4px;
+  box-shadow: var(--box-shadow);
+  h1 {
     display: flex;
-    flex-direction: column;
-    
-    padding: 38px 18px 24px 18px;
-    gap: 10px;
+    justify-content: center;
+  }
+  button {
+    margin-top: 8px;
+    margin-bottom: 8px;
 
-    background-color: var(--color-medium-green);
+    background: var(--color-button-two);
+    color: var(--white);
+    font-weight: 600;
 
-    border-radius: 4px; 
-    box-shadow: var(--box-shadow);
-    h1{
-        display: flex;
-        justify-content: center;
+    border: 1px solid var(--color-primary-dark);
+    border-radius: 4px;
+
+    width: 259.9px;
+    height: 38.5px;
+
+    &:hover {
+      background-color: var(--color-button-home-hover);
     }
-    button {
-        margin-top: 8px;
-        margin-bottom: 8px;
+  }
+  input {
+    background: #262626;
+    color: var(--white);
+    font-weight: 600;
 
-        background: var(--color-light-green);
-        color: var(--color-primary-dark);
-        font-weight: 600;
+    align-items: center;
 
-        border: 1px solid var(--color-light-green);
-        border-radius: 4px;
+    padding: 0.3rem;
 
-        width: 259.9px;
-        height: 38.5px;
+    border: 1px solid var(--color-primary-dark);
+    border-radius: 4px;
 
+    width: 264.66px;
+    height: 38.5px;
+
+    &::placeholder {
+      color: var(--white);
+      font-weight: 600;
     }
-    input {
-        background: var(--color-light-green);
-        color: var(--color-primary-dark);
-        font-weight: 600;
-
-        align-items: center;
-
-        padding: .3rem;
-
-        border: 1px solid var(--color-light-green);
-        border-radius: 4px;
-
-        width: 264.66px;
-        height: 38.5px;
-        
-        &::placeholder {
-        
-            color: var(--color-primary-dark);
-            font-weight: 600;
-        }
-    }
-`
+  }
+`;
 export const ContainerMessage = styled.div`
-    text-align: center;
-    width: 100%;
-    strong {
-        font-weight: 500;
-    }
-`
+  text-align: center;
+  width: 100%;
+  strong {
+    font-weight: 500;
+  }
+`;
