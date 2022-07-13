898dd8fd91eba93476050e30baba04bb4b181be9
Gabi <gabriellemedeiros51@gmail.com>
2022-07-13 14:52:54 -0300

    Fix: Arrumando rotas do login para aparecer após login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,21 +3,18 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 import { useHistory } from "react-router-dom";
-import{ useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { Redirect } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 import schema from "./formYup";
-import {api} from "../../services/api";
-
+import { api } from "../../services/api";
 
 import { Container, Form, ContainerMessage } from "./style";
 import Input from "../../components/Input";
 
-
 const Login = () => {
-
-  const [ auth, setAuth ] =  useState(false);
+  const [auth, setAuth] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("@TrashNoFood:token");
@@ -26,11 +23,10 @@ const Login = () => {
       return setAuth(true);
     }
   }, []);
- 
 
-  const { 
-    register, 
-    handleSubmit, 
+  const {
+    register,
+    handleSubmit,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
@@ -38,41 +34,41 @@ const Login = () => {
 
   const history = useHistory();
 
-  if(auth) {
-    return <Redirect to="/dashboard"/>;
+  if (auth) {
+    return <Redirect to="/vitrine" />;
   }
 
   const handleLogin = async (data) => {
-    const response = await api.post("/login", data).catch((err) => { 
-      console.log(err);  
+    const response = await api.post("/login", data).catch((err) => {
+      console.log(err);
       toast.error("Ops! algo não está certo");
     });
 
     const { user, accessToken } = response.data;
 
-      localStorage.setItem("@TrashNoFood:token", accessToken);
-      localStorage.setItem("@TrashNoFood:id", JSON.stringify(user.id));
+    localStorage.setItem("@TrashNoFood:token", accessToken);
+    localStorage.setItem("@TrashNoFood:id", JSON.stringify(user.id));
 
-      toast.success("Login feito");
+    toast.success("Login feito");
 
-      setAuth(true)
-      history.push("/dashboard")
-  }
+    setAuth(true);
+    history.push("/vitrine");
+  };
 
   return (
     <Container>
       <Form onSubmit={handleSubmit(handleLogin)}>
         <h1>Login</h1>
-        <Input 
+        <Input
           register={register}
-          name="email" 
+          name="email"
           label="Email"
           placeholder="Digite aqui seu email"
           error={errors.email?.message}
         />
-        <Input 
-          register={register} 
-          name="password" 
+        <Input
+          register={register}
+          name="password"
           label="Senha"
           type="password"
           placeholder="Digite aqui sua senha"
@@ -81,11 +77,13 @@ const Login = () => {
         <button type="submit">Entrar</button>
         <ContainerMessage>
           <strong>Ainda não possui uma conta?</strong>
-        </ContainerMessage> 
-        <button type="button" onClick={() => history.push('/register')}>Cadastre-se</button>
+        </ContainerMessage>
+        <button type="button" onClick={() => history.push("/register")}>
+          Cadastre-se
+        </button>
       </Form>
     </Container>
-  )
-}
+  );
+};
 
 export default Login;
