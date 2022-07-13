97f4da062c313c9039baed0bfcbce45e1b4c37c7
Lais Oliveira <laiscarvoliveira@gmail.com>
2022-07-13 19:38:56 -0300

    fix: dark mode toast

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@ import { GlobalStyle } from "./styles/global";
 import Routes from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {AnimatePresence} from "framer-motion"
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
     <div className="App">
       <AnimatePresence>
-      <GlobalStyle />
-      <Routes />
+        <GlobalStyle />
+        <Routes />
 
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          theme="dark"
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </AnimatePresence>
     </div>
   );
